a99fe7cc67db10e0ba159149d9791d226334c29e
dwoob <130300743+dwoob@users.noreply.github.com>
2023-04-28 01:12:02 +0900

    Add files via upload

---
+++ api-server/server.js
@@ -0,0 +1,24 @@
+const config = require('./config')[process.env.NODE_ENV];
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const port = config.PORT;
+const cors = require('cors');
+
+//cors
+let corsOptions = {
+	origin: '*',
+	credential: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended : true }));
+
+const autoRoute = require('./autoRoute');
+autoRoute('/api',app);
+
+const webServer = http.createServer(app);
+webServer.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+})
